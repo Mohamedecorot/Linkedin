ba344913425cd24bbb2a40e7cbaba0a05331b1d3
Mohamedecorot <60646768+Mohamedecorot@users.noreply.github.com>
2022-03-28 14:55:58 +0200

    03_L'essentiel du javascript : 01_Découvrir js : 06_Commenter son code js

---
+++ 03_L'essentiel du javascript/01_Découvrir js/06_Commenter son code js/simple.js
@@ -0,0 +1,5 @@
+/*Aller rechercher l'élément h1
+portant l'id "heading" dans la page*/
+var heading = document.getElementById('heading');
+//Mettre du contenu dans cet élément h1
+heading.innerHTML = "Une simple page HTML";
