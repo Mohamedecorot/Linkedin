daa293d116a0cbbb76f0345a57795960c01b868e
Mohamedecorot <60646768+Mohamedecorot@users.noreply.github.com>
2022-04-11 08:45:01 +0200

    03_L'essentiel du javascript : 03_Aborder les boucles et les conditions : 02_Tirer parti de l'opérateur ternaire

---
+++ 03_L'essentiel du javascript/03_Aborder les boucles et les conditions/02_Tirer parti de l'opérateur ternaire/simple.js
@@ -0,0 +1,11 @@
+var age = 15;
+
+// question ? valeur si vrai : valeur si faux ;
+
+age >= 18 ? console.log(`À ${age} ans, vous êtes majeur.`) : console.log(`À ${age} ans, vous êtes encore mineur.`);
+
+// if(age >= 18){
+//     console.log(`À ${age} ans, vous êtes majeur.`);
+// } else {
+//     console.log(`À ${age} ans, vous êtes encore mineur.`);
+// }
