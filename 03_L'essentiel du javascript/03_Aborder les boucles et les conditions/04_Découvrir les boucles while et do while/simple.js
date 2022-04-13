68815a7c7d0b052b236aaeb1b4d2f951bafed9f4
Mohamedecorot <60646768+Mohamedecorot@users.noreply.github.com>
2022-04-13 23:46:56 +0200

    03_L'essentiel du javascript : 03_Aborder les boucles et les conditions : 04_Découvrir les boucles while et do while

---
+++ 03_L'essentiel du javascript/03_Aborder les boucles et les conditions/04_Découvrir les boucles while et do while/simple.js
@@ -0,0 +1,13 @@
+var a = 1;
+
+while(a < 10){
+    console.log(`la valeur de la variable est ${a}.`)
+    a++;
+}
+
+do {
+    console.log(`la valeur de la variable est ${a}.`)
+    a++;
+} while (a < 10)
+
+console.log("Boucle terminée !")
