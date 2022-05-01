e95cc7f1aacf8f937bd0176f3e5b30cdb3a9e7e3
Mohamedecorot <60646768+Mohamedecorot@users.noreply.github.com>
2022-05-01 15:00:55 +0200

    03_L'essentiel du javascript : 06_Découvrir le DOM : 03_Utiliser queryselector et queryselectorAll

---
+++ 03_L'essentiel du javascript/06_Découvrir le DOM/03_Utiliser queryselector et queryselectorAll/simple.js
@@ -0,0 +1,5 @@
+let selected = document.querySelector('#mainHeader');
+let pFooter = document.querySelector('#mainFooter p');
+let list = document.querySelectorAll('#mainNav li');
+
+console.log(selected);
