5921d3fc3f8c223c42c89e89e6d9d535fda51348
Mohamedecorot <60646768+Mohamedecorot@users.noreply.github.com>
2022-05-01 15:02:44 +0200

    03_L'essentiel du javascript : 06_Découvrir le DOM : 02_Acceder aux elements du dom

---
+++ 03_L'essentiel du javascript/06_Découvrir le DOM/02_Acceder aux elements du dom/simple.js
@@ -0,0 +1,7 @@
+let header = document.getElementById('mainHeader');
+let footer = document.getElementById('mainFooter');
+let active = document.getElementsByClassName('active');
+let lien = document.getElementsByTagName('a');
+let pFooter = footer.getElementsByTagName('p');
+
+console.log(header);
