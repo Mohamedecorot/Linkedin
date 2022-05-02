aeea8b532705f2be9cc6c06bd20c15dbf111c680
Mohamedecorot <60646768+Mohamedecorot@users.noreply.github.com>
2022-05-02 15:15:44 +0200

    03_L'essentiel du javascript : 06_Découvrir le DOM : 04_Modifier les éléments du dom

---
+++ 03_L'essentiel du javascript/06_Découvrir le DOM/04_Modifier les éléments du dom/simple.js
@@ -0,0 +1,6 @@
+let main = document.querySelector('#pageContent main');
+let mainP = document.querySelector('#pageContent main p');
+mainP.setAttribute('align', 'right');
+mainP.align= "right";
+
+console.log(`L'element main possede ${main.children.length} enfants`);
